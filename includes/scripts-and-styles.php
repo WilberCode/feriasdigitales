64f0c75f8cb6ab0a1af996a30d85711043b0303f
WilberCode <parionahuaraca@gmail.com>
2020-05-05 13:48:46 -0500

    cambios typo

--- includes/scripts-and-styles.php
+++ includes/scripts-and-styles.php
@@ -11,13 +11,13 @@ add_action( 'wp_enqueue_scripts', function () {
  	wp_register_script( 'app', AssetResolver::resolve( 'js/app.js' ), [], false, true );
  	  // enqueue global assets
 	wp_enqueue_style('fontawesome',"https://use.fontawesome.com/releases/v5.0.13/css/all.css"); 
-	wp_enqueue_style('font-roboto',"https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"); 
+	wp_enqueue_style('font-roboto',"https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,300;1,400&display=swap"); 
    
 	wp_enqueue_script( 'jquery');
 	wp_enqueue_style( 'app' ); 
 	wp_enqueue_script( 'app' );    
 
-	// Api rest
+	// Api restuiol
 
 	wp_localize_script( 'app', 'ajax_marcas', array(
         'url'    => rest_url( '/marcas/destacados' ),
