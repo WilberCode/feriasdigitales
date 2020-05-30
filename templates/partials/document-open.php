8cdcfe08f96f390ad9c8e381ec0f9543da56fc4b
WilberCode <parionahuaraca@gmail.com>
2020-05-29 22:55:43 -0500

    cambios

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= wp_get_document_title() ?></title>
-    
+    <title><?= wp_get_document_title() ?></title> 
     <script>document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');</script>
     <meta name="google-site-verification" content="WDw7jVppxrNYVp4G8Y8qv8XgyIe0JybUlpUqOVUJv28" />
-    <meta name="keywords" content="Grupo Tendenze Comunicaciones, Ferias digitales, promoción, dia del padre, dia del madre, feria digital mamá, feria digital papá, muestra con tu marca, participa con una promoción"> 
+    <meta name="keywords" content="Grupo Tendenze Comunicaciones, Ferias digitales, promoción, dia del padre, dia del madre, feria digital mamá, feria digital papá, muestra con tu marca, participa con una promoción, Belleza, Deco Vintage, Seguros, Nutrición, Maternidad, Accesorios, Video mamá, Detalles, Postres, Flores, 
+Meat Fest, Carnes, Gourmet, Piel Seca, Experiencia, Vinos, Meditación"> 
     <?php wp_head() ?>    
    
     <!-- Global site tag (gtag.js) - Google Analytics -->
