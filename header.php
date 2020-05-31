8f80d897d767613a293ef01a2673e281e8b42949
WilberCode <parionahuaraca@gmail.com>
2020-05-31 15:35:09 -0500

    nuevos cambios

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
          <?php if(is_page('1ra-feria-digital-mama')){?>
                     <nav class="header-nav" >
                         <ul class="header-menu">
-                            <li><a href="<?php echo home_url(); ?>" class="hidden md:block" >2da Feria Digital PAPÁ</a></li>
+                            <li><a href="<?php echo home_url(); ?>" class="hidden md:block" >Feria Digital Junio 2020</a></li>
                         </ul>
                     </nav>
                 <?php } else{ ?> 
