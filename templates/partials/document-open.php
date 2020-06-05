4f42a20598b19d91d09e8506b5ceca4e33d81172
WilberCode <parionahuaraca@gmail.com>
2020-06-05 10:56:07 -0500

    cambios layout

--- templates/partials/document-open.php
+++ templates/partials/document-open.php
@@ -2,8 +2,8 @@
 <html lang="ES" class="no-js">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= wp_get_document_title() ?></title> 
     <script>document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/, 'js');</script>
     <meta name="google-site-verification" content="WDw7jVppxrNYVp4G8Y8qv8XgyIe0JybUlpUqOVUJv28" />
