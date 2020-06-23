7424b2ebe91220ab9f964816f021a0ea44ca1f29
WilberCode <parionahuaraca@gmail.com>
2020-06-23 10:56:26 -0500

    cambios

--- footer.php
+++ footer.php
@@ -2,7 +2,9 @@
  <?php if(!is_page('como-participar')){?>
     <footer  class="bg-black w-full py-6 " > 
     <div  class="container" >
-        <p  class="text-white text-xs font-medium text-justify text-gray-400 tracking-wider  " >La 1ra Feria Digital es una iniciativa solidaria para apoyar sin costo a empresas que necesitan vender online sus servicios respetando la cuarentena. La modalidad de entrega del producto o servicio se hará con gift card digitales o via delivery propio autorizado de la misma empresa participante. Nuestro apoyo solo es a nivel de difusión no nos responsabilizamos por las transacciones entre empresa y comprador final así como de la entrega del regalo. #YoMeQuedoEnCasa - Organiza: Grupo Tendenze Comunicaciones</p> 
+        <p  class="text-white text-xs font-medium text-justify text-gray-400 tracking-wider">“Ferias Digitales” nace en época del COVID-19 y es una iniciativa organizada por Grupo Tendenze Comunicaciones para apoyar a empresas que necesitan vender online sus servicios respetando las normas de seguridad. Nuestro apoyo es a nivel de difusión. Cada marca es responsable de la transacción entre empresa y comprador final así como de la entrega del producto o servicio. Cada participación se renueva cada mes y esperamos que más marcas emprendedoras o independientes se vayan sumando.
+Juntos y trabajando con todas las herramientas digitales disponibles, el comercio ¡saldrá adelante!</p>
+        <p  class="text-white text-xs font-medium text-justify text-gray-400 tracking-wider">Juntos y trabajando con todas las herramientas digitales disponibles, el comercio ¡saldrá adelante!</p>
     </div>
  </footer>
  <?php }?>
