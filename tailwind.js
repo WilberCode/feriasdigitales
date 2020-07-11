605dbf282c7839f1d6b0c79691a59c85f6d8f72e
WilberCode <parionahuaraca@gmail.com>
2020-07-11 12:13:04 -0500

    cambios styles

--- tailwind.js
+++ tailwind.js
@@ -74,6 +74,20 @@ module.exports = {
             lineHeight: {
                 tighter: '1.125',
             }, 
+            maxWidth: {
+                xs: '20rem',
+                sm: '24rem',
+                md: '28rem',
+                lg: '32rem',
+                xl: '36rem',
+                '2xl': '42rem',
+                '3xl': '48rem',
+                '4xl': '56rem',
+                '5xl': '64rem',
+                '6xl': '72rem',
+                'modal': '66rem',
+                full: '100%',
+            },
             fontFamily: {
                 // sans: [
                 //     '-apple-system',
